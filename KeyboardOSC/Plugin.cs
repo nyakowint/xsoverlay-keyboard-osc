74eee6537bcbad4c4c95bbe6f2de38c6d3598968
Nyako <24845294+nyakowint@users.noreply.github.com>
2023-12-28 19:07:56 -0500

    Bump version

--- KeyboardOSC/Plugin.cs
+++ KeyboardOSC/Plugin.cs
@@ -18,7 +18,7 @@ namespace KeyboardOSC
     [BepInPlugin("nwnt.keyboardosc", "KeyboardOSC", AssemblyVersion)]
     public class Plugin : BaseUnityPlugin
     {
-        public const string AssemblyVersion = "1.2.1.0";
+        public const string AssemblyVersion = "1.2.2.0";
         public static Plugin Instance;
         public static ManualLogSource PluginLogger;
 
